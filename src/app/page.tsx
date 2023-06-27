0a7426c1b501b1d5548f76cfeafc9aed15e42276
TeXmeijin <i8858garden@gmail.com>
2023-06-27 23:33:26 +0900

    fix: 🐛 seo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,26 @@
 import { Index } from "@/components/screens";
+import Head from "next/head";
 
 function App() {
-  return <Index></Index>;
+  return (
+    <main>
+      <Head>
+        <meta
+          name={"viewport"}
+          content={"width=device-width, initial-scale=1"}
+        />
+        <meta charSet={"utf-8"} />
+        <title>Color Code Guesser</title>
+        <meta
+          name={"description"}
+          content={
+            "Color Code Guesser - This is a simple game of guessing the color code of the displayed color."
+          }
+        />
+      </Head>
+      <Index></Index>
+    </main>
+  );
 }
 
 export default App;
